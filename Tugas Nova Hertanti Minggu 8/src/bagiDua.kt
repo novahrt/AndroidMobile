c69f0b8868110b8f040fcd31c66b0336ae50312d
nova.hrt <novahertanti@gmail.com>
2024-04-15 13:16:39 +0700

    Tugas Minggu ke 8

---
+++ Tugas Nova Hertanti Minggu 8/src/bagiDua.kt
@@ -0,0 +1,17 @@
+fun bagiDua(pembilang: Int, penyebut: Int) {
+    try {
+        val hasil = pembilang / penyebut
+        println("Hasil pembagian: $hasil")
+    } catch (e: ArithmeticException) {
+        println("Terjadi kesalahan: ${e.message}")
+    }
+}
+
+fun main() {
+    // Contoh pemanggilan fungsi bagiDua
+    val pembilang = 20
+    val penyebut = 2 // Ubah nilai penyebut agar tidak nol
+
+    println("Mencoba membagi $pembilang dengan $penyebut")
+    bagiDua(pembilang, penyebut)
+}
