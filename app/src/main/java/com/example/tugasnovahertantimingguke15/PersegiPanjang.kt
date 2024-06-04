4b5e7a53b225ab0454237111c9615fd5710f5db9
nova.hrt <novahertanti@gmail.com>
2024-06-04 20:44:18 +0700

    TugasMinggu15

---
+++ app/src/main/java/com/example/tugasnovahertantimingguke15/PersegiPanjang.kt
@@ -0,0 +1,7 @@
+package com.example.tugasnovahertantimingguke15
+
+class PersegiPanjang(val panjang: Int, val lebar: Int) {
+    fun hitungLuas(): Int {
+        return panjang * lebar
+    }
+}
---
+++ app/src/test/java/com/example/tugasnovahertantimingguke15/PersegiPanjangTest.kt
@@ -0,0 +1,30 @@
+package com.example.tugasnovahertantimingguke15
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class PersegiPanjangTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun hitungLuas() {
+    }
+
+    @Test
+    fun getPanjang() {
+    }
+
+    @Test
+    fun getLebar() {
+    }
+}
